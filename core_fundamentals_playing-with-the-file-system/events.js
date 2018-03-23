d217893eeec9e7e3c5e6280f697464cb9b67bb81
Frank Simon <f.m.sim@t-online.de>
2018-03-23 18:10:20 +0100

    setting new folder, added file-reading.js and file-writing.js

---
+++ core_fundamentals_playing-with-the-file-system/events.js
@@ -0,0 +1,6 @@
+const events = require('events');
+let emitter = new events.EventEmitter();
+emitter.on("newEvent", (message)=>{
+  console.log(`Message: ${message}`);
+});
+emitter.emit("newEvent", "Hello guys this is edwin");
---
+++ core_fundamentals_playing-with-the-file-system/global.js
@@ -0,0 +1,6 @@
+var path = require("path");
+var name = "Edwin Diaz";
+var newName = name.toUpperCase(name);
+console.log(`newName is ${newName}`);
+console.log(__dirname);
+console.log(path.basename(__filename));
