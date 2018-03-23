dc99cc0309b36cd16bceea8f8582f983d94d8096
Frank Simon <f.m.sim@t-online.de>
2018-03-23 21:20:12 +0100

    added http-request.js and data.json

---
+++ core_fundamentals-HTTP-NPM_and_more/http-request.js
@@ -0,0 +1,25 @@
+const https = require('https');
+const fs    = require('fs');
+
+const url = 'https://jsonplaceholder.typicode.com/posts';
+
+https.get(url, res =>{
+  res.setEncoding('utf8');
+  let body = '';
+  res.on('data', data =>{
+    body += data;
+  });
+  res.on('end', ()=>{
+    /* showing data in the console
+    body = JSON.parse(body);
+    console.log(`
+      ${body[0].title}
+    `);
+    */
+    /* catching data and write it into a file */
+    fs.writeFile('data.json', body, 'utf8', (err)=>{
+      if (err) return err;
+      console.log("Just pulled all the posts and created file");
+    });
+  });
+});
