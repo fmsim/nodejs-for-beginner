6529ce88d2cd112136bbd9311f912397656f6904
Frank Simon <f.m.sim@t-online.de>
2018-03-28 21:53:55 +0200

    added http-serving-json.js

---
+++ core_fundamentals-HTTP-NPM_and_more/http-serving-json.js
@@ -0,0 +1,21 @@
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+
+const url = 'https://jsonplaceholder.typicode.com/posts';
+
+http.createServer((req, serverRes) => {
+  if (req.method === 'GET' && req.url === '/posts') {
+    https.get(url, (httpRes) => {
+      httpRes.on('data', data => {
+        httpRes.setEncoding('utf8');
+        console.log(data)
+      });
+      httpRes.on('end', () => {
+        serverRes.end();
+        console.log("Its over");
+      });
+    });
+  }
+}).listen(4444);
+console.log("Server is listening");
