ec5aa4aafbf6247f5ab1e781f0e63a5ef29a9b9e
Frank Simon <f.m.sim@t-online.de>
2018-03-28 22:13:57 +0200

    modified http-serving-json.js

--- core_fundamentals-HTTP-NPM_and_more/http-serving-json.js
+++ core_fundamentals-HTTP-NPM_and_more/http-serving-json.js
@@ -9,13 +9,17 @@ http.createServer((req, serverRes) => {
     https.get(url, (httpRes) => {
       httpRes.on('data', data => {
         httpRes.setEncoding('utf8');
-        console.log(data)
+        //console.log(data);
+        serverRes.write(data);
       });
       httpRes.on('end', () => {
         serverRes.end();
         console.log("Its over");
       });
     });
-  }
+  } else {
+      serverRes.writeHead(404, {'Content-Type': 'text/plain'});
+      serverRes.end("404 ERROR, could not find what you were looking for")
+    }
 }).listen(4444);
 console.log("Server is listening");
